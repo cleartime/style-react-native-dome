ba590a0182e7f0daa6f8d308d5b1a50aa108b9f5
cleartime <562606139@qq.com>
2016-09-04 18:43:42 +0800

    fetch

--- src/component/MyScene.js
+++ src/component/MyScene.js
@@ -2,19 +2,36 @@
  * Created by gxx on 16/9/3.
  */
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableHighlight } from 'react-native';
 
 export default class MyScene extends Component {
-    static get defaultProps() {
-        return {
-            title: 'MyScene'
-        };
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: '点击我',
+        }
     }
+
+    fetchData = (ca) => {
+        fetch('http://cleartime.leanapp.cn/linkfriend')
+            .then((response) => response.json())
+            .then((responseJson) => {
+                this.setState({
+                    title: responseJson.data,
+                })
+            }).catch((error) => {
+            console.error(error);
+        });
+    };
+
 
     render() {
         return (
             <View>
-                <Text>Hi! My name is {this.props.title}.</Text>
+                <Text>my frist dome</Text>
+                <TouchableHighlight onPress={this.fetchData}>
+                    <Text>点击我</Text>
+                </TouchableHighlight>
             </View>
         )
     }
